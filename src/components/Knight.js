1fe21885bb359a103642065653f8107a4d1c7ae6
Fumio Nonaka <fumiononaka@FumioNonaka.local>
2020-02-10 00:31:30 +0900

    React DnD 01

---
+++ src/components/Knight.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const knightStyle = {
+	fontSize: 60,
+	fontWeight: 'bold',
+	cursor: 'move',
+};
+const Knight = () => {
+	return (
+		<div style={knightStyle}>
+			♘
+		</div>
+	);
+}
+
+export default Knight;
